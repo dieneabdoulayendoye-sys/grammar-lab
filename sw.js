d20fe98eb304f3ed4d76a0b4224dd789d7f6e3ff
dieneabdoulayendoye-sys <dieneabdoulayendoye@gmail.com>
2026-09-04 14:15:37 +0000

    Add bilingual cognate coach

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "grammar-lab-v3";
+const CACHE_NAME = "grammar-lab-v4";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
